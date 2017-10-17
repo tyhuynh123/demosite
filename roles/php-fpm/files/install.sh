97cfa8c2089e57aca11ef2a5bcc20deb6134e78f
Ty Huynh <thuynh@verimatrix.com>
2017-10-16 17:24:17 -0700

    first commit

---
+++ roles/php-fpm/files/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/tyhuynh123/demosite.git
+
+cd demosite && cp * -R /var/www/html/
+
+mkdir -p /var/www/html/gallery-images && chmod 777 /var/www/html/gallery-images
+mkdir -p /var/www/html/resources/cache && chmod 777 /var/www/html/resources/cache
+
+apt-get install composer -y
+
+cd /var/www/html/ && composer install
+
+sudo service apache2 restart
