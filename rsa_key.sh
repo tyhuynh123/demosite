77994eb0abbf89f7fa40dd0d21d58b7319f0b208
tyhuynh123 <tyhuynh123@yahoo.com>
2018-09-12 12:53:18 -0700

    Create rsa_key.sh

---
+++ rsa_key.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Create RSA key variable  
+rsakey=$(cat ~/.ssh/id_rsa.pub)
+
+## Adding RSA key to user_data 
+echo "$rsakey >> /home/ansible/.ssh/authorized_keys" >> roles/aws_create_instance/templates/user_data 
+
+## Adding sshd restart to user_data
+echo "service sshd restart" >> roles/aws_create_instance/templates/user_data
