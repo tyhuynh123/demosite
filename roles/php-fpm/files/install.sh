264bbfd419aacb122439e21b25f617a6d268a6e5
tyhuynh123 <tyhuynh123@yahoo.com>
2019-11-11 13:28:46 -0800

    Update install.sh

--- roles/php-fpm/files/install.sh
+++ roles/php-fpm/files/install.sh
@@ -7,10 +7,4 @@ cd demofiles && cp * -R /var/www/html/
 mkdir -p /var/www/html/gallery-images && chmod 777 /var/www/html/gallery-images
 mkdir -p /var/www/html/resources/cache && chmod 777 /var/www/html/resources/cache
 
-apt-get install composer -y
-
-cd /var/www/html/ 
-
-composer install
-
 sudo service apache2 restart
